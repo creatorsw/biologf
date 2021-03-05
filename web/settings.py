8ced3203fc9d289f5eba3511fd03dcd0c3997d7b
creatorsw <77376925+creatorsw@users.noreply.github.com>
2021-03-05 15:47:18 +0530

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'chalukya.reddy.9@gmail.com'
