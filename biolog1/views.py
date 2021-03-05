3f73cb24fbd456658804a3893d91b90aafc7f624
creatorsw <77376925+creatorsw@users.noreply.github.com>
2021-03-05 15:17:26 +0530

    Add files via upload

---
+++ biolog1/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def blog(request):
+    return render(request, "blog.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def send(request):
+    if request.method == 'POST':
+        Name = request.POST.get('Name', False)
+        Email = request.POST.get('Email', False)
+        text = request.POST.get('text', False)
+
+        send_mail(
+            Name,
+            text,
+            Email,
+            ['chalukya.reddy.7@gmail.com'],
+            fail_silently=False,
+        )
+
+        send_mail(
+            'WELCOME TO BIOLOG',
+            'THANK YOU FOR CONTACTING US',
+            Email,
+            [Email],
+            fail_silently=False,
+        )
+        return render(request, 'index.html', {'Name': Name})
+    else:
+        return render(request, 'index.html', {})
